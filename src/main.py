2ce97340986513697b4ac058fd7ab8c1e2bfaeab
nakano16180 <nakano16180@gmail.com>
2019-06-23 15:58:32 +0900

    元に戻した

--- src/main.py
+++ src/main.py
@@ -8,25 +8,15 @@ from kobuki_msgs.msg import WheelDropEvent
 
 
 def emergency_callback(message):
-	print message.state
-	if int(message.state) == 1:
-		p2=subprocess.Popen(['rosnode','list'], stdout=subprocess.PIPE)
-		p2.wait()
-		nodelist=p2.communicate()
-		#print nodelist
-		nd=nodelist[0]
-		nd=nd.split("\n")
-		print nd
-		for i in range(len(nd)):
-			subprocess.call(['rosnode','kill',nd[i]]) 
-		
-
+    print message.state
+    if int(message.state) == 1:
+        os.system('kill `ps aux | grep ros | awk \'{print $2}\'`')
 
 def main():
-	rospy.init_node('emergency_node')
-	rospy.Subscriber('/mobile_base/events/wheel_drop', WheelDropEvent, emergency_callback)
-	rospy.spin()
+    rospy.init_node('emergency_node')
+    rospy.Subscriber('/mobile_base/events/wheel_drop', WheelDropEvent, emergency_callback)
+    rospy.spin()
 
 
 if __name__ == '__main__':
-	main()
+    main()
