d969104e3dafe248d1b591e7af8feec9777c570a
Nenetti <akinori1090@gmail.com>
2019-07-03 05:57:00 +0900

    [update] プロセス削除時にプロセス削除の音声を追加

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 
 import rospy
+import rospkg
 from kobuki_msgs.msg import WheelDropEvent
 
 
@@ -12,10 +13,17 @@ def emergency_callback(message):
     if int(message.state) == 1:
         os.system('kill `ps aux | grep ros | awk \'{print $2}\'`')
 
+    file = rospkg.RosPack().get_path('emergency_stop') + "/voice.wav"
+    create_command = ["pico2wave", "-w=" + file, "All, processes, delete"]
+    play_command = ["aplay", file]
+    subprocess.call(create_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.call(play_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 def main():
     rospy.init_node('emergency_node')
     rospy.Subscriber('/mobile_base/events/wheel_drop', WheelDropEvent, emergency_callback)
     rospy.spin()
+
 
 
 if __name__ == '__main__':
